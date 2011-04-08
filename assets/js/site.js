20e2c41733214472001e1c5a2bf47312a870dc06
John Faulds <registrations@tyssendesign.com.au>
2011-04-08 13:58:28 +1000

    Added head.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,34 +1,27 @@
-$(document).ready(function() {
-	
+head.ready(function() {
+
 	$.preloadCssImages();
 	
-	// google analytics
-	//var pageTracker = _gat._getTracker("UA-2575846-1");
-	pageTracker._initData();
-	pageTracker._trackPageview();
-
 	$('a[rel="external"]').click( function() {
         window.open($(this).attr('href') );
         return false;
     });
 
-	$('.header .nav').superfish({ 
+	$('.menu > ul').superfish({ 
 		delay: 1000,
 		animation: {opacity:'show',height:'show'},
-		speed: 500,
+		speed: 300,
 		autoArrows: false
 	});
 	
 	if($('body').is('.home')) {
-		$('.flash').flash(
-			{ src: '/images/title.swf', width: 760, height: 450, wmode: 'transparent' },
-			{ update: false }
-		);
 	}
-	if($('body').is('.p-events')) {
-		$(".fancy a").fancybox();
-	}
+
 	if($('body').is('.p-contact')) {
 		$("#contact").validate();
 	}
+	
+	!window.jQuery && document.write(unescape('%3Cscript src="/assets/js/jquery-1.4.2.min.js"%3E%3C/script%3E'))
 });
+
+head.js("/assets/js/modernizr-1.6.min.js","http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js","/assets/js/jquery.preload-5.js","/assets/js/superfish.js");
