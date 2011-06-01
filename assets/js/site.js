70e65da29e2d5fee6a50c518e36a64d9321bec27
John Faulds <registrations@tyssendesign.com.au>
2011-06-01 10:57:57 +1000

    Updated with styles from http://necolas.github.com/normalize.css/ and jQuery Placeholder from https://github.com/danbentley/placeholder

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,39 @@
+(function($) {
+    /**
+     * Spoofs placeholders in browsers that don't support them. (eg Firefox 3)
+     * Copyright 2011 Dan Bentley
+     * Licensed under the Apache License 2.0
+     * Author: Dan Bentley [github.com/danbentley]
+     */
+
+    // Return if native support is available.
+    if ("placeholder" in document.createElement("input")) return;
+
+    function setupPlaceholder(input) {
+        var placeholderText = input.attr('placeholder');
+        if (input.val() === '') input.val(placeholderText);
+        input.bind('focus blur', function(e) {
+            if (e.type === 'focus' && input.val() === placeholderText) input.val(''); 
+            if (e.type === 'blur' && input.val() === '') input.val(placeholderText); 
+        });
+    }
+
+    function clearPlaceholdersBeforeSubmit(form) {
+        form.find(':input[placeholder]').each(function() {
+            var el = $(this);
+            if (el.val() === el.attr('placeholder')) el.val('');
+        });
+    }
+ 
+    $(':input[placeholder]').each(function(index) {
+        setupPlaceholder($(this));
+    });
+   
+    $('form').submit(function(e) {
+        clearPlaceholdersBeforeSubmit($(this));
+    });
+})(jQuery);
+
 head.ready(function() {
 
 	$.preloadCssImages();
