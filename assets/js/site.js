4f6c6c10776d97f981cf236239dc926af80f8234
John Faulds <registrations@tyssendesign.com.au>
2011-12-02 10:52:42 +1000

    Updated jQuery
    to 1.7.1
    Removed Helium and GGS
    Removed some unnecessary stuff from master.less

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,47 +1,47 @@
-(function($) {
-    /**
-     * Spoofs placeholders in browsers that don't support them. (eg Firefox 3)
-     * Copyright 2011 Dan Bentley
-     * Licensed under the Apache License 2.0
-     * Author: Dan Bentley [github.com/danbentley]
-     */
-
-    // Return if native support is available.
-    if ("placeholder" in document.createElement("input")) return;
-
-    function setupPlaceholder(input) {
-        var placeholderText = input.attr('placeholder');
-        if (input.val() === '') input.val(placeholderText);
-        input.bind('focus blur', function(e) {
-            if (e.type === 'focus' && input.val() === placeholderText) input.val(''); 
-            if (e.type === 'blur' && input.val() === '') input.val(placeholderText); 
-        });
-    }
-
-    function clearPlaceholdersBeforeSubmit(form) {
-        form.find(':input[placeholder]').each(function() {
-            var el = $(this);
-            if (el.val() === el.attr('placeholder')) el.val('');
-        });
-    }
- 
-    $(':input[placeholder]').each(function(index) {
-        setupPlaceholder($(this));
-    });
-   
-    $('form').submit(function(e) {
-        clearPlaceholdersBeforeSubmit($(this));
-    });
-})(jQuery);
-
 head.ready(function() {
 
+	(function($) {
+		/**
+		 * Spoofs placeholders in browsers that don't support them. (eg Firefox 3)
+		 * Copyright 2011 Dan Bentley
+		 * Licensed under the Apache License 2.0
+		 * Author: Dan Bentley [github.com/danbentley]
+		 */
+
+		// Return if native support is available.
+		if ("placeholder" in document.createElement("input")) return;
+
+		function setupPlaceholder(input) {
+			var placeholderText = input.attr('placeholder');
+			if (input.val() === '') input.val(placeholderText);
+			input.bind('focus blur', function(e) {
+				if (e.type === 'focus' && input.val() === placeholderText) input.val(''); 
+				if (e.type === 'blur' && input.val() === '') input.val(placeholderText); 
+			});
+		}
+
+		function clearPlaceholdersBeforeSubmit(form) {
+			form.find(':input[placeholder]').each(function() {
+				var el = $(this);
+				if (el.val() === el.attr('placeholder')) el.val('');
+			});
+		}
+	 
+		$(':input[placeholder]').each(function(index) {
+			setupPlaceholder($(this));
+		});
+	   
+		$('form').submit(function(e) {
+			clearPlaceholdersBeforeSubmit($(this));
+		});
+	})(jQuery);
+	
 	$.preloadCssImages();
 	
 	$('a[rel="external"]').click( function() {
-        window.open($(this).attr('href') );
-        return false;
-    });
+		window.open($(this).attr('href') );
+		return false;
+	});
 
 	$('.menu > ul').superfish({ 
 		delay: 1000,
@@ -68,7 +68,7 @@ head.ready(function() {
 		$(".photos a").fancybox();
 	}
 	
-	window.jQuery || document.write('<script src="/assets/js/jquery-1.6.1.min.js">\x3C/script>')
+	window.jQuery || document.write('<script src="/assets/js/jquery-1.7.min.js">\x3C/script>')
 });
 
-head.js("http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js","/min/f=/assets/js/modernizr-2.0.min.js,/assets/js/respond.min.js,/assets/js/jquery.preload-5.js,/assets/js/hoverIntent.js,/assets/js/superfish.js,/assets/js/nivo.js,/assets/js/jquery.fancybox.js&20110519","http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.pack.js");
+head.js("http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js","/min/f=/assets/js/modernizr-2.0.min.js,/assets/js/respond.min.js,/assets/js/jquery.preload-5.js,/assets/js/hoverIntent.js,/assets/js/superfish.js,/assets/js/nivo.js,/assets/js/jquery.fancybox.js&20110519","http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.pack.js");
