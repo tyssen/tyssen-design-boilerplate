df9c70e4cb7b7b21f66c3aa373d5bf7c7ca7992e
John Faulds <registrations@tyssendesign.com.au>
2012-08-03 23:15:23 +1000

    Replaced Head.js with LAB.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,62 +1,79 @@
-head.ready(function() {
+$LAB
+	.script("/assets/js/modernizr-2.5.0.min.js")
+	.script("//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js").wait()
+	.script("/assets/js/jquery.preload-5.js")
+	// .script("/assets/js/hoverIntent.js")
+	// .script("/assets/js/superfish.js")
+	.wait(function(){
 
-	(function($) {
-		/**
-		 * Spoofs placeholders in browsers that don't support them. (eg Firefox 3)
-		 * Copyright 2011 Dan Bentley
-		 * Licensed under the Apache License 2.0
-		 * Author: Dan Bentley [github.com/danbentley]
-		 */
+		$.preloadCssImages();
 
-		// Return if native support is available.
-		if ("placeholder" in document.createElement("input")) return;
+		// $('.sf-menu').superfish({ 
+		// 	delay: 1000,
+		// 	animation: {opacity:'show',height:'show'},
+		// 	speed: 300,
+		// 	autoArrows: false
+		// });
 
-		function setupPlaceholder(input) {
-			var placeholderText = input.attr('placeholder');
-			if (input.val() === '') input.val(placeholderText);
-			input.bind('focus blur', function(e) {
-				if (e.type === 'focus' && input.val() === placeholderText) input.val(''); 
-				if (e.type === 'blur' && input.val() === '') input.val(placeholderText); 
-			});
-		}
-
-		function clearPlaceholdersBeforeSubmit(form) {
-			form.find(':input[placeholder]').each(function() {
-				var el = $(this);
-				if (el.val() === el.attr('placeholder')) el.val('');
-			});
-		}
-	 
-		$(':input[placeholder]').each(function(index) {
-			setupPlaceholder($(this));
+		// Open links in new windows/tabs
+		
+		$('a[rel="external"]').click( function() {
+			window.open($(this).attr('href') );
+			return false;
 		});
-	   
-		$('form').submit(function(e) {
-			clearPlaceholdersBeforeSubmit($(this));
+
+		// Scroll to internal anchors
+
+		$('[href="#content"]').click(function() {
+			$('html, body').animate({
+				scrollTop: ($($(this).attr("href")).offset().top - 20) + "px"
+			}, {
+				duration: 1000,
+				easing: "swing"
+			});
+			return false;
 		});
-	})(jQuery);
-	
-	$.preloadCssImages();
-	
-	$('a[rel="external"]').click( function() {
-		window.open($(this).attr('href') );
-		return false;
-	});
 
-	$("a[href^='#']").not("a[href]='#'").click(function() {
-		$("#"+$(this).attr("href").slice(1)+"").focus();
-	});
+		// Show menu link for screens smaller than 600px
 
-	$('.menu > ul').superfish({ 
-		delay: 1000,
-		animation: {opacity:'show',height:'show'},
-		speed: 300,
-		autoArrows: false
-	});
-	
-	if($('body').is('.contact')) {
-		$("#contact").validate();
-	}
-});
+		// if(Modernizr.mq('only screen and (max-width: 599px)')) {
+		// 	$('.header header').append('<a href="#nav" id="show-menu">Menu <span>&#x25BC;</span></a>');
+		// }
 
-head.js("http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js","/min/f=/assets/js/jquery.preload-5.js,/assets/js/hoverIntent.js,/assets/js/superfish.js,/assets/js/ios-orientationchange-fix.js","http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.pack.js");
+	})
+	// Home
+	.script(function(){
+		if ($('body').hasClass('home')) {
+			return "http://cloud.github.com/downloads/malsup/cycle/jquery.cycle.all.latest.js";
+		}
+	})
+	.wait(function(){
+		$('.slides').cycle({
+			fx:      'fade',
+			speed:   2000,
+			timeout: 8000,
+			// pager:      '.pager', 
+			// pagerEvent: 'mouseover', 
+			pauseOnPagerHover: true
+		});
+	})
+	// Study pages
+	.script(function(){
+		if ($('body').hasClass('study')) {
+			return "/assets/js/jquery.fancybox.pack.js";
+		}
+	})
+	.wait(function(){
+		$("article ul a").fancybox({
+			width 		: '90%',
+			fitToView	: true,
+			autoSize	: true,
+			closeClick	: false,
+			openEffect	: 'fade',
+			closeEffect	: 'fade',
+			type		: 'ajax'
+		});
+	})
+
+	// .script("/assets/new/js/helper.js")
+;
