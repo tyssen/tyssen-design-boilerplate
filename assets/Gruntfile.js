2da5bad836e0686bd9b19e5c23898f1f6875eeda
John Faulds <registrations@tyssendesign.com.au>
2013-11-26 13:56:17 +1000

    Updated Grunt file based on http://coding.smashingmagazine.com/2013/10/29/get-up-running-grunt/

--- assets/Gruntfile.js
+++ assets/Gruntfile.js
@@ -1,30 +1,92 @@
-module.exports = function(grunt) {
+module.exports = function(grunt){
+
+    "use strict";
+   require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
     grunt.initConfig({
+
         pkg: grunt.file.readJSON('package.json'),
-        watch: {
-            sass: {
-                files: ['sass/**/*.{scss,sass}','sass/components/**/*.{scss,sass}'],
-                tasks: ['sass:dist']
-            },
-            livereload: {
-                files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css','img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+
+        cssc: {
+            build: {
                 options: {
-                    livereload: true
+                    consolidateViaDeclarations: true,
+                    consolidateViaSelectors:    true,
+                    consolidateMediaQueries:    true
+                },
+                files: {
+                    'assets/css/master.css': 'assets/css/master.css'
                 }
             }
         },
+
+        cssmin: {
+            build: {
+                src: 'assets/css/master.css',
+                dest: 'assets/css/master.css'
+            }
+        },
+
         sass: {
-            dist: {
+            build: {
                 files: {
-                    'css/master.css': 'sass/master.scss'
+                    'assets/css/master.css': 'assets/sass/master.scss'
+                }
+            }
+        },
+
+        watch: {
+            html: {
+                files: ['index.html'],
+                tasks: ['htmlhint']
+            },
+            js: {
+                files: ['assets/js/site.js'],
+                tasks: ['uglify']
+            },
+            css: {
+                files: ['assets/sass/**/*.scss'],
+                tasks: ['buildcss']
+            }
+        },
+
+        htmlhint: {
+            build: {
+                options: {
+                    'tag-pair': true,
+                    // Force tags to have a closing pair
+                    'tagname-lowercase': true,
+                    // Force tags to be lowercase
+                    'attr-lowercase': true,
+                    // Force attribute names to be lowercase e.g. <div id="header"> is invalid
+                    'attr-value-double-quotes': true,
+                    // Force attributes to have double quotes rather than single
+                    'doctype-first': true,
+                    // Force the DOCTYPE declaration to come first in the document
+                    'spec-char-escape': true,
+                    // Force special characters to be escaped
+                    'id-unique': true,
+                    // Prevent using the same ID multiple times in a document
+                    'head-script-disabled': true,
+                    // Prevent script tags being loaded in the  for performance reasons
+                    'style-disabled': true
+                    // Prevent style tags. CSS should be loaded through
                 },
-                // options: {
-                //     sourcemap: 'true'
-                // }
+                src: ['index.html']
+            }
+        },
+
+        uglify: {
+            build: {
+                files: {
+                    'assets/js/base.min.js': ['assets/js/site.js']
+                }
             }
         }
+
     });
-    grunt.registerTask('default', ['watch']);
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default',   []);
+    grunt.registerTask('buildcss',  ['sass', 'cssc', 'cssmin']);
+
 };
